933e6ab6516a424424d5863df4b55e11cf4aa46f
vivekgujari <vivekgujari@gmail.com>
2024-10-23 17:39:47 -0600

    added mvc project

---
+++ Authentication_And_Authorization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Authentication_And_Authorization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
